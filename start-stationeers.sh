5398a8e5cb5e6dfc014dd0579d022401a5507a8d
David Andersen <git@davidandersen.us>
2019-05-15 21:52:49 +0000

    use variables

--- start-stationeers.sh
+++ start-stationeers.sh
@@ -2,7 +2,7 @@ source /etc/stationeers/stationeers.conf
 docker stop stationeers
 docker rm stationeers
 docker run -d \
-  -v /efs/stationeers/saves:/home/steam/stationeers/saves \
+  -v $SAVE_DIR:/home/steam/stationeers/saves \
   -v /var/lib/stationeers:/home/steam/stationeers \
   -v /efs/stationeers/default.ini:/home/steam/stationeers/default.ini \
   -p 27500:27500/udp -p 27500:27500 -p 27015:27015/udp \
