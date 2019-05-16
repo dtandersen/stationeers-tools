6ece2fc17c1426c47c3b4641035dee832529bab6
David Andersen <git@davidandersen.us>
2019-05-16 10:38:03 -0700

    write to log

--- start-stationeers.sh
+++ start-stationeers.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
 source /etc/stationeers/stationeers.conf
+WORLD=$SAVE
 docker stop stationeers
 docker rm stationeers
+LOGFILE=/efs/stationeers/logs/stationeers-${WORLD}.log
+touch "$LOGFILE"
 docker run -d \
   -v $SAVE_DIR:/home/steam/stationeers/saves \
   -v /var/lib/stationeers:/home/steam/stationeers \
   -v /efs/stationeers/default.ini:/home/steam/stationeers/default.ini \
+  -v "${LOGFILE}:/home/steam/stationeers/stationeers.log" \
   -p 27500:27500/udp -p 27500:27500 -p 27015:27015/udp \
   -e BRANCH=$BRANCH \
   --restart unless-stopped \
