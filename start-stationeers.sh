f8cd2b59cc00b23dd7e7a43ec5fc418ac9ecb77f
David Andersen <git@davidandersen.us>
2019-05-16 12:11:18 -0700

    preserve block list

--- start-stationeers.sh
+++ start-stationeers.sh
@@ -9,6 +9,7 @@ docker run -d \
   -v $SAVE_DIR:/home/steam/stationeers/saves \
   -v /var/lib/stationeers:/home/steam/stationeers \
   -v /efs/stationeers/default.ini:/home/steam/stationeers/default.ini \
+  -v /efs/stationeers/blocked.txt:/home/steam/stationeers/blocked.txt \
   -v "${LOGFILE}:/home/steam/stationeers/stationeers.log" \
   -p 27500:27500/udp -p 27500:27500 -p 27015:27015/udp \
   -e BRANCH=$BRANCH \
