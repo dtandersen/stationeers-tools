760ec6541168f0f1f393ea757f13d057861db77f
David Andersen <git@davidandersen.us>
2019-05-18 21:45:37 -0700

    removed junk items

--- restart-stationeers.sh
+++ restart-stationeers.sh
@@ -17,4 +17,7 @@ stop-stationeers.sh
 stationeers-map-trimmer -m /efs/stationeers/saves/$SAVE/world.bin -x=0 -y=0 --radius=250
 rm -f /efs/stationeers/saves/$SAVE/world.bin
 mv /efs/stationeers/saves/$SAVE/world.bin.trimmed /efs/stationeers/saves/$SAVE/world.bin
+java -cp /opt/saxon/saxon9he.jar net.sf.saxon.Transform -s:/efs/stationeers/saves/$SAVE/world.xml -xsl:/opt/stationeers-xslt-filter/server-clean2.xslt -o:/efs/stationeers/saves/$SAVE/world.new.xml
+rm -f /efs/stationeers/saves/$SAVE/world.xml
+mv /efs/stationeers/saves/$SAVE/world.new.xml /efs/stationeers/saves/$SAVE/world.xml
 start-stationeers.sh
